e493debd2da08b20ca123a0ce41a1d36fd8d37f9
miravuong <158006280+miravuong@users.noreply.github.com>
2025-12-23 05:13:04 +0000

    Deploying to gh-pages from @ miravuong/portfolio@ea7c32bd476d56b34a26f4ff9b2c353ef61d7d4e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "a collection of projects i&#39;ve worked on ⋆˙⟡",
           section: "Navigation",
           handler: () => {
             window.location.href = "/portfolio/projects/";
@@ -409,51 +409,21 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-group-bot-signature",
+          title: 'Group Bot Signature',
+          description: "Prototype implementation of HTTP Message Signatures using BBS group signatures for privacy-preserving bot authentication and accountability.",
           section: "Projects",handler: () => {
               window.location.href = "/portfolio/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-adversarial-robustness-testing-of-cnns-with-fgsm",
+          title: 'Adversarial Robustness Testing of CNNs with FGSM',
+          description: "Implemented a Fast Gradient Sign Method (FGSM) adversarial attack in PyTorch by training a convolutional neural network (CNN) and generating imperceptibly perturbed inputs that caused controlled misclassification, strengthening understanding of ML model vulnerabilities and adversarial-robust defense.",
           section: "Projects",handler: () => {
               window.location.href = "/portfolio/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-cookie-jar",
+          title: 'Cookie Jar',
+          description: "An educational web app that teaches kids about website cookies and internet privacy through interactive games.",
           section: "Projects",handler: () => {
               window.location.href = "/portfolio/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
